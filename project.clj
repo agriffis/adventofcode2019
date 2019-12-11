c0040698c8e5e81dd243e8536fc62dd6bd59553b
Aron Griffis <aron@scampersand.com>
2019-12-10 23:40:26 -0500

    kill old dep

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Public Domain" :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.int-map "0.2.4"]
-                 [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :test-paths ["src" "test"])
