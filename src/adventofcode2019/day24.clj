aa462250b0f7b3a64e5ae33afd41b95fb59e12dc
Aron Griffis <aron@scampersand.com>
2019-12-25 06:59:15 -0500

    day 24 part 2

--- src/adventofcode2019/day24.clj
+++ src/adventofcode2019/day24.clj
@@ -12,29 +12,25 @@
   [s]
   (->> s (filter cells) (map cells) (partition (.indexOf s "\n")) array))
 
-(defn mget*
-  ([a pos] (mget* a pos 0))
-  ([a pos default]
-   (try
-     (apply mget a pos)
-     (catch IndexOutOfBoundsException e default))))
+(defn frame
+  ([a] (frame a 0 0 0 0))
+  ([a top right bottom left]
+   (as-> a $
+     (join-along 0 (broadcast top [1 5]) $ (broadcast bottom [1 5]))
+     (join-along 1 (broadcast left [7 1]) $ (broadcast right [7 1])))))
 
-(defn pad
+(defn unframe
   [a]
-  (as-> (zero-array (map (partial + 2) (shape a))) a'
-    (emap-indexed (partial mget* a) a')
-    (shift a' [-1 -1])))
+  (array (submatrix a 1 (- (row-count a) 2) 1 (- (column-count a) 2))))
 
-(defn unpad
-  [a]
-  (submatrix a 1 (- (row-count a) 2) 1 (- (column-count a) 2)))
+(defn fate
+  [x & neighbors]
+  (let [sum (int (apply + neighbors))]
+    (if (zero? x) (if (< 0 sum 3) 1 0) (if (= 1 sum) 1 0))))
 
-(defn live-or-die
+(defn step-
   [a]
-  (emap (fn [x & others]
-          (if (zero? x)
-            (if (< 0 (apply + others) 3) 1 0)
-            (if (== 1 (apply + others)) 1 0)))
+  (emap fate
         a
         (shift a [1 0])
         (shift a [0 1])
@@ -43,31 +39,101 @@
 
 (defn step
   [a]
-  (-> (live-or-die a)
-      (set-row 0 0)
-      (set-row (dec (row-count a)) 0)
-      (set-column 0 0)
-      (set-column (dec (column-count a)) 0)))
+  (-> a frame step- unframe))
 
 (defn powers-of
   [n]
   (iterate (partial * n) 1))
 
+(def powers-matrix (->> (powers-of 2) (partition 5) (take 5) array))
+
 (defn biodiversity
   [a]
-  (->> (unpad a) (mapcat (partial map int)) (map * (powers-of 2)) (apply + 0)))
+  (long (esum (emul a powers-matrix))))
 
 (defn draw
   [a]
   (pm a {:formatter (comp str [\. \#] int)}))
 
 (defn part1
-  []
-  (let [a (pad (parse input))
-        as (iterate step a)
-        seens (reductions conj #{} as)
-        dup (first (->> (map vector as seens)
-                        (drop-while (fn [[a seen]] (not (seen a))))
-                        (map first)))]
-    (draw dup)
-    (biodiversity dup)))
+  ([] (part1 input))
+  ([input]
+   (let [a (parse input)
+         as (iterate step a)
+         bs (map biodiversity as)
+         seens (reductions conj #{} bs)
+         [dup bio] (first (->> (map vector as bs seens)
+                               (drop-while (fn [[a b seen]] (not (seen b))))))]
+     (draw dup)
+     bio)))
+
+(def dpad {:up [2 1] :right [3 2] :down [2 3] :left [1 2] :center [2 2]})
+
+(defn dget
+  [a d]
+  (let [[x y] (dpad d)]
+    (mget a y x)))
+
+(defn dset
+  [a d v]
+  (let [[x y] (dpad d)]
+    (mset a y x v)))
+
+(def zeroes (zero-matrix 5 5))
+
+(defn step2-
+  [higher a lower]
+  (-> (apply frame a (map (partial dget higher) [:up :right :down :left]))
+      step-
+      unframe
+      (dset :up
+            (apply fate
+              (dget a :up)
+              (mget a 1 1)
+              (mget a 0 2)
+              (mget a 1 3)
+              (get-row lower 0)))
+      (dset :right
+            (apply fate
+              (dget a :right)
+              (mget a 1 3)
+              (mget a 2 4)
+              (mget a 3 3)
+              (get-column lower 4)))
+      (dset :down
+            (apply fate
+              (dget a :down)
+              (mget a 3 1)
+              (mget a 4 2)
+              (mget a 3 3)
+              (get-row lower 4)))
+      (dset :left
+            (apply fate
+              (dget a :left)
+              (mget a 1 1)
+              (mget a 2 0)
+              (mget a 3 1)
+              (get-column lower 0)))
+      (dset :center 0)))
+
+(defn step2
+  [aa]
+  (cond-> aa
+    (some pos? (eseq (first aa))) (as-> $ (concat [zeroes] $))
+    (some pos? (eseq (peek aa))) (as-> $ (concat $ [zeroes]))
+    true (as-> $ (mapv step2-
+                   (concat [zeroes] (drop-last $))
+                   $
+                   (concat (rest $) [zeroes])))))
+
+(defn count-bugs
+  [aa]
+  (apply + (map (comp long esum) aa)))
+
+(defn part2
+  ([] (part2 input))
+  ([input]
+   (let [a (parse input)
+         aas (iterate step2 [a])
+         final (nth aas 200)]
+     (count-bugs final))))
